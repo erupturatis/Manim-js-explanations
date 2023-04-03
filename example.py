c34fa6a82b94dd64a5af583ee6bf3efba1ff7c62
erupturatis <bb.octav@gmail.com>
2023-04-03 10:23:29 +0300

    (progress)[Mon 04/03/2023]  found lib for exact usecase

---
+++ example.py
@@ -0,0 +1,15 @@
+# example.py
+
+from manim import *
+# or: from manimlib import *
+from manim_slides import Slide
+
+class BasicExample(Slide):
+    def construct(self):
+        circle = Circle(radius=3, color=BLUE)
+        dot = Dot()
+
+        self.play(GrowFromCenter(circle))
+        self.next_slide()  # Waits user to press continue to go to the next slide
+
+        self.play(Write(Text("Hello World").scale(1)))
