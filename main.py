63fdb6f8f3d18d78d08c3e8e5cf05679f58bf695
erupturatis <bb.octav@gmail.com>
2023-04-04 12:46:08 +0300

    (progress)[Tue 04/04/2023]  proj basic structure

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-
 from manim import *
 # or: from manimlib import *
 from manim_slides import Slide
 
 class BasicExample(Slide):
+
+    
     def smallwait(self):
         self.wait(0.3)
     
     def construct(self):
+        SLIDES = [self.introduction,  self.dom]
+
+        for slide in SLIDES:
+            Vmobjects = slide()
+            self.play(FadeOut(Vmobjects)) 
+            self.clear()
+
+    def introduction(self):
         circle = Circle(radius=3.85, color=BLUE)
         hello = Text("Hello  world")
         java = Text("Javascript").set_color_by_gradient(RED, ORANGE, RED)
@@ -56,3 +65,34 @@ class BasicExample(Slide):
         self.play(Indicate(warnings[0], color=RED, scale_factor=1.5))
         self.play(Write(warnings[1:]))
         self.end_loop()
+        return VGroup(warnings )
+
+    
+    def dom(self):
+        rect = Rectangle(width=6, height=4, color=BLUE)
+        text = Text("Body Text").scale(1.5)
+        rect_text = VGroup(rect, text)
+        rect_text.arrange(DOWN)
+        rect_text.set_width(6)
+        rect_text.set_height(3)
+        rect_text.move_to(ORIGIN)
+
+        # Create three divs nested below the rectangle
+        div1 = Rectangle(width=2, height=1, color=YELLOW)
+        id1 = Text("id1").scale(0.7).next_to(div1, DOWN)
+        div1_id = VGroup(div1, id1).next_to(rect_text, DOWN, buff=0.5)
+
+        div2 = Rectangle(width=2, height=1, color=YELLOW)
+        id2 = Text("id2").scale(0.7).next_to(div2, DOWN)
+        div2_id = VGroup(div2, id2).next_to(div1_id, DOWN, buff=0.5)
+
+        div3 = Rectangle(width=2, height=1, color=YELLOW)
+        id3 = Text("id3").scale(0.7).next_to(div3, DOWN)
+        div3_id = VGroup(div3, id3).next_to(div2_id, DOWN, buff=0.5)
+
+        # Add everything to the scene
+        self.play(Create(rect_text))
+        self.play(Create(div1_id))
+        self.play(Create(div2_id))
+        self.play(Create(div3_id))
+        self.wait()
