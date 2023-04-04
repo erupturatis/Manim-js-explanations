2341288ead5ac0b0f8fb2987691484cbfc4fa100
Iulian <iulian.stratan55@gmail.com>
2023-04-04 12:07:16 +0300

    🎨 Introduction and warning

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from manim_slides import Slide
 
 class BasicExample(Slide):
     def smallwait(self):
-        self.wait(0.5)
+        self.wait(0.3)
     
     def construct(self):
         circle = Circle(radius=3.85, color=BLUE)
@@ -19,14 +19,18 @@ class BasicExample(Slide):
                 Text("Animations")                         
                     ).arrange(DOWN, buff=1.5).scale(0.75)
         intro = VGroup(
-                Text("Introduction").scale(2.1),
-                Text("JavaScript is to Java as ham is to a hamster."),
+                Text("Introduction").scale(2.5),
+                Text('"JavaScript is to Java as ham is to a hamster."'),
                 Text("JavaScript != Java"),
                 Text("JavaScript is a lightweight, interpreted programming language."),
                 Text("It is designed for creating network-centric applications."),
-                Text("It is complimentary to and integrated with Java."),
                 Text("It is easy to learn. \U0001F609"),
-                    ).arrange(DOWN, buff=1.5).scale(0.75)
+                    ).arrange(DOWN, buff=0.55).scale(0.5)
+        warnings = VGroup(
+                Text("Warning!").scale(2.9),
+                Text("Oamenii slabi de inima sa nu se uite la ecran pentru restul prezentarii!"),
+                Text("Ati fost avertizati!"),
+                    ).arrange(DOWN, buff=0.55).scale(0.5)
 
 
         self.play(GrowFromCenter(circle))
@@ -40,3 +44,15 @@ class BasicExample(Slide):
         self.play(FadeOut(circle), FadeOut(java), FadeOut(authors))
         self.clear()
         self.play(Write(content,  run_time=4))
+        self.next_slide()
+
+        self.play(ReplacementTransform(content, intro, run_time=0.5))
+        self.next_slide()
+
+        self.play(FadeOut(intro))
+        self.clear()
+        self.play(Write(warnings[0]))
+        self.start_loop()
+        self.play(Indicate(warnings[0], color=RED, scale_factor=1.5))
+        self.play(Write(warnings[1:]))
+        self.end_loop()
